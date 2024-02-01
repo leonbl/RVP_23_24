8acb44bd6fae963f97ad560a5427c36a449ef629
leonbl <leon.blazic@gmail.com>
2024-02-01 17:01:57 +0100

    pid

--- Motor_PID/src/main.cpp
+++ Motor_PID/src/main.cpp
@@ -9,6 +9,16 @@ bool direction = 0;
 uint8_t state = 0;
 uint8_t old_state = 0;
 int32_t counter = 0;
+uint32_t period = 100;
+
+// PID
+float setpoint = 100, e=0, e_prev = 0, e_sum = 0;
+float Kp = 1, Kd = 0, Ki = 0;
+float KP = Kp;
+float KD = Kd / (float)period;
+float KI = Ki * (float)period;
+float u = 0, up = 0, ui = 0, ud = 0;
+
 
 // Create an instance of 'IFX007TMotorControl' called 'MyMotor'
 IFX007TMotorControl MyMotor = IFX007TMotorControl();
@@ -27,8 +37,18 @@ void setup()
 
 void loop()
 {
-  Serial.println(counter);
-  delay(10);
+  uint32_t curTime = millis();
+  while(millis() < (curTime + period));
+  Serial.println(millis());
+  // Izračun PID
+  e = setpoint - counter;
+  up = KP * e;
+  e_sum += e;
+  ui = KI * e_sum;
+  ud = KD * (e - e_prev);
+  u = up + ui + ud;
+  e_prev = e;
+  MyMotor.setBiDirMotorSpeed(direction, u);
 }
 
 void encoder()
