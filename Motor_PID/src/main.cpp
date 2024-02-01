5b324964835cdeb9e248ef96d194dce408a0935a
leonbl <leon.blazic@gmail.com>
2024-02-01 17:37:59 +0100

    deluje

--- Motor_PID/src/main.cpp
+++ Motor_PID/src/main.cpp
@@ -12,13 +12,12 @@ int32_t counter = 0;
 uint32_t period = 100;
 
 // PID
-float setpoint = 100, e=0, e_prev = 0, e_sum = 0;
-float Kp = 1, Kd = 0, Ki = 0;
+float setpoint = 100, e = 0, e_prev = 0, e_sum = 0;
+float Kp = 0.5, Kd = 0, Ki = 0.4;
 float KP = Kp;
-float KD = Kd / (float)period;
-float KI = Ki * (float)period;
+float KD = Kd /((float)period / 1000);
+float KI = Ki * (float)period/1000;
 float u = 0, up = 0, ui = 0, ud = 0;
-
 
 // Create an instance of 'IFX007TMotorControl' called 'MyMotor'
 IFX007TMotorControl MyMotor = IFX007TMotorControl();
@@ -29,7 +28,7 @@ void setup()
 {
   pinMode(encA, INPUT);
   pinMode(encB, INPUT);
-  Serial.begin(9600);
+  Serial.begin(115200);
   attachInterrupt(digitalPinToInterrupt(encA), encoder, RISING);
   MyMotor.begin();
   MyMotor.setBiDirMotorSpeed(direction, speed);
@@ -38,8 +37,8 @@ void setup()
 void loop()
 {
   uint32_t curTime = millis();
-  while(millis() < (curTime + period));
-  Serial.println(millis());
+  while (millis() < (curTime + period))
+    ;
   // Izračun PID
   e = setpoint - counter;
   up = KP * e;
@@ -48,12 +47,31 @@ void loop()
   ud = KD * (e - e_prev);
   u = up + ui + ud;
   e_prev = e;
+
+  if (u > 0)
+  {
+    direction = 0;
+  }
+  else
+  { 
+    u = abs(u);
+    direction = 1;
+  }
+
+  if(u > 255) u = 255;
+
   MyMotor.setBiDirMotorSpeed(direction, u);
+  Serial.print("u: ");
+  Serial.print(u);
+  Serial.print(" dir: ");
+  Serial.print(direction);
+  Serial.print(" count: ");
+  Serial.println(counter);
 }
 
 void encoder()
 {
-  if(digitalRead(encB))
+  if (digitalRead(encB))
     counter++;
   else
     counter--;
